3a093ef98453d61fb96dd0e59e12e8be25513819
Updater <erlend@klakegg.net>
2020-02-05 14:21:07 +0100

    Adding some basic Schematron scripts for non-Windows installations.

--- tools/package.sh
+++ tools/package.sh
@@ -1,14 +1,23 @@
 #!/bin/sh
 
-TARGET=$(pwd)/target
+PROJECT=$(pwd) # Yes, hack!
+TARGET=$PROJECT/target
 DIST=$1
-GROUP=$1
+GROUP=$2
 
 mkdir -p $TARGET/pkg
+test -e $TARGET/pkg/$DIST.zip && rm -rf $TARGET/pkg/$DIST.zip
 
-for d in common $DIST $GROUP; do
-  if [ -e $TARGET/dist/$d ]; then
-    cd $TARGET/dist/$d
-    zip -q9r $TARGET/pkg/$DIST.zip *
-  fi
+for a in "static" "target/dist"; do
+  for d in common $GROUP $DIST; do
+    if [ -e $PROJECT/$a/$d ]; then
+      cd $PROJECT/$a/$d
+
+      if [ -e bin ]; then
+        chmod a+x bin/*
+      fi
+
+      zip -q9r $TARGET/pkg/$DIST.zip *
+    fi
+  done
 done
