7a3222c0982c0890bb3f165ee3f9352555932cbb
Erlend Klakegg Bergheim <erlend@klakegg.net>
2020-08-06 22:12:35 +0200

    Adding "pdk ruby" command.

---
+++ static/common/lib/peppol/shell/command/ruby.rb
@@ -0,0 +1,4 @@
+script = ARGV[0]
+ARGV.shift
+
+require File.expand_path script, Dir.pwd
--- static/win/bin/pdk.bat
+++ static/win/bin/pdk.bat
@@ -3,5 +3,6 @@
 SET PDK_PATH=%~dp0
 SET PDK_PATH=%PDK_PATH:\bin\=%
 SET JAVA_HOME=%PDK_PATH%\lib\corretto
+SET SAXON_HOME=%PDK_PATH%\lib\saxon
 
 "%PDK_PATH%\lib\jruby\bin\jruby.exe" "%PDK_PATH%\lib\peppol\shell\main.rb" %*
